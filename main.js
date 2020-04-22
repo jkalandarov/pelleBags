259961333850e865b927c3705ceb973705151e5e
Jasurbek Kalandarov <62233209+jkalandarov@users.noreply.github.com>
2020-04-22 23:04:09 +0500

    add files

---
+++ main.js
@@ -0,0 +1,11 @@
+//Select element function
+const selectElement = element => {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', ()=> {
+    body.classList.toggle('open');
+})
